bb13fda637e226f5d794cf777aba913f3c24cad9
Saif Ali <sachinsaif03@gmail.com>
2023-11-23 00:36:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/saifalisew1508",
+    "https://github.com/TeamAviax/AviaxMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
